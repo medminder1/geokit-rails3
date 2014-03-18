35ce60718523b004bdc33843e78406d54d583930
medminder <udi@medminder.com>
2014-03-18 22:21:08 +0200

    Build gem.

---
+++ geokit-rails3/lib/geokit/rails3.rb
@@ -0,0 +1,7 @@
+require "geokit/rails3/version"
+
+module Geokit
+  module Rails3
+    # Your code goes here...
+  end
+end
---
+++ geokit-rails3/lib/geokit/rails3/version.rb
@@ -0,0 +1,5 @@
+module Geokit
+  module Rails3
+    VERSION = "0.0.1"
+  end
+end
